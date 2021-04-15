e8ce28972543b5bc9a677b4371ce84308bd68f67
ArinaS14 <arinas14@mail.ru>
2021-04-15 11:33:17 +0500

    start project

---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+let gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+    gulp.src('app/scss/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('app/css'))
+});
